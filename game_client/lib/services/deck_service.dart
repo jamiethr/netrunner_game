08fec2a9c2923d1fd602482ad0e3fa250e94012f
jamiethr <jamie.throup@gmail.com>
2022-11-01 06:22:41 -0700

    Finished basic API implementation as per the tutorial. Started appying API in DeckService.dart

--- game_client/lib/services/deck_service.dart
+++ game_client/lib/services/deck_service.dart
@@ -4,6 +4,14 @@ import 'package:game_client/services/api_service.dart';
 
 class DeckService extends ApiService {
   
+/// note to self: Tyler said "Every time I make an API call, it's going to be a Future." Why?
+///               "It's going to be a Future because it's async."
 
+/// Builds a new deck.
+///
+/// Use this when starting a new game.
+/// Code from Tyler Codes on YouTube:
+/// https://www.youtube.com/c/TylerCodes
+Future<void> newDeck()
 
 }
